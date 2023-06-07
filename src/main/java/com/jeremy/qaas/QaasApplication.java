c0c7351d2acc8725db68debce522209f4f0ae560
“Jeremy.wangc” <2236005339@qq.com>
2023-06-07 08:46:36 +0800

    学位论文算法验证平台 后端系统搭建 第一次提交

---
+++ src/main/java/com/jeremy/qaas/QaasApplication.java
@@ -0,0 +1,13 @@
+package com.jeremy.qaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeremy/qaas/QaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremy.qaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
